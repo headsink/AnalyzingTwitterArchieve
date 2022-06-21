82367eb38b3d522ceb9e760e271f7f8e4a9897cb
headsink <headsink@gmail.com>
2022-06-21 16:26:24 +0800

    parse args

--- analyze-tweet.py
+++ analyze-tweet.py
@@ -1,1 +1,8 @@
+import argparse
+
 if __name__ == 'main':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-j","--js",dest="js_file", help="tweet.js file inside the archive folder", metavar="FILE")
+    args = parser.parse_args()
+
+    
