5a2903937bceb18985b892e07611cf7cfd0e37a4
headsink <headsink@gmail.com>
2022-06-21 17:41:51 +0800

    changes

--- analyze-tweet.py
+++ analyze-tweet.py
@@ -1,8 +1,12 @@
 import argparse
 
-if __name__ == 'main':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-j","--js",dest="js_file", help="tweet.js file inside the archive folder", metavar="FILE")
     args = parser.parse_args()
 
-    
+
+    if args.js_file is None:
+        print("No argument. Enter argument -h or --help for more information")
+        exit(1)
+
